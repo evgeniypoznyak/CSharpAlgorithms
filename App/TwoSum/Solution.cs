d7cb68dae44a7d8d588c6b435a63c048c1f5af5f
Evgeniy Poznyak <evgeniy.poznyak@gmail.com>
2020-10-25 02:53:28 -0500

    Initial commit

---
+++ App/TwoSum/Solution.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+
+namespace App.TwoSum
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            if (nums.Length == 0)
+            {
+                return new int[] { };
+            }
+            
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new[] {i, j};
+                    }
+                }
+            }
+            
+            return new int[] { };
+            // var numAndIndex = new Dictionary<int, int>();
+            // for(int i = 0; i < nums.Length; i++)
+            // {
+            //     var curNumber = nums[i];
+            //     var numberA = target - curNumber;
+            //
+            //     if (numAndIndex.ContainsKey(numberA))
+            //     {
+            //         return new int[] {numAndIndex[numberA], i};
+            //     }
+            //
+            //     numAndIndex[curNumber] = i;
+            // }
+            // return null;
+        }
+    }
+}
