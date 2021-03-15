000c2ef5c772f2d9208415288cc2e3dd8d98749c
evgeniy.poznyak <evgeniy.poznyak@motorolasolutions.com>
2021-03-15 14:44:05 -0500

    solved

---
+++ App/LeetCode/DefangingAnIPAddress/DefangingAnIPAddress.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace App.LeetCode.DefangingAnIPAddress
+{
+    public class Solution {
+        public string DefangIPaddr(string address)
+        {
+            Regex pattern = new Regex("[.]");
+           return pattern.Replace(address, "[.]");
+        }
+    }
+}
---
+++ AppTests/LeetCodeTests/DefangingAnIPAddressTests/DefangingAnIPAddressTests.cs
@@ -0,0 +1,32 @@
+﻿using App.LeetCode.DefangingAnIPAddress;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace AppTests.LeetCodeTests.DefangingAnIPAddressTests
+{
+    public class DefangingAnIpAddressTests
+    {
+        private readonly ITestOutputHelper output;
+        
+        [Theory] 
+        [MemberData(nameof(TestData))]
+        public void DefangIPaddrTest(string num,  string expected)
+        {
+            var actual = new Solution().DefangIPaddr(num);
+            Assert.Equal(expected, actual);
+        } 
+        
+        // The method can return IEnumerable<object[]>
+        // You can use TheoryData to strongly type the result
+        public static TheoryData<string, string> TestData()
+        {
+            var data = new TheoryData<string, string>
+            {
+                {"1.1.1.1", "1[.]1[.]1[.]1"},
+                {"255.100.50.0", "255[.]100[.]50[.]0"}
+            };
+            return data;
+        }
+    }
+}
